1c140609a6ef8a9caac8ce3edeab071ee93bcbae
Muammer AKILLI <muammerakilli@gmail.com>
2020-07-15 16:31:21 +0300

    Proje Başlangıç Onayı

---
+++ SqlMultiScript/Forms/ExecF.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SqlMultiScript.Forms
+{
+    public partial class ExecF : Form
+    {
+        public ExecF()
+        {
+            InitializeComponent();
+        }
+    }
+}
